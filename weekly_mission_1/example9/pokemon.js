235e597fc2d4b688b2418785dc659b9117216c86
EduardoSanchezHz <eduardoszhdez@outlook.com>
2022-04-16 15:00:47 -0500

    Agregando ejercicios practicos

--- weekly_mission_1/example9/pokemon.js
+++ weekly_mission_1/example9/pokemon.js
@@ -7,8 +7,9 @@ class Pokemon {
         console.log(`Mi pokemon ${this.name} te saluda!!!`)
     }
 
-
-
+    sayMessage() {
+        console.log(`Mi pokemon ${this.name} te dice: Heey!`)
+    }
 }
 
 module.exports = Pokemon;
